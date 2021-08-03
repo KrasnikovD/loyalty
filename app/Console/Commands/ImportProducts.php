7b3d6ea8788d03452f44be2b995ec36caea66ae4
Lhc Boinc <lhc_boinc@yahoo.com>
2021-08-03 11:25:41 +0300

    import products

--- app/Console/Commands/ImportProducts.php
+++ app/Console/Commands/ImportProducts.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Categories;
+use App\Models\Product;
 use Illuminate\Console\Command;
 
 class ImportProducts extends Command
@@ -11,7 +13,7 @@ class ImportProducts extends Command
      *
      * @var string
      */
-    protected $signature = 'import_products {f} {c?}';
+    protected $signature = 'import_products {f}';
 
     /**
      * The console command description.
@@ -38,11 +40,59 @@ class ImportProducts extends Command
     public function handle()
     {
         $file = $this->argument('f');
-        $IsParseCategory = $this->argument('c');
-        $sourcesData = json_decode(file_get_contents($file));
-        if ($IsParseCategory) {
-            $categories = array_values(array_unique(array_column($sourcesData, 'category')));
-            
+        $categoriesMap = [];
+        foreach (Categories::select('categories.id', 'categories.name')->get() as $item) {
+            $categoriesMap[$item->name] = $item->id;
+        }
+        $productsList = [];
+        foreach (Product::all() as $item) {
+            $productsList[$item->code] = $item;
+        }
+        $registeredCodes = array_keys($productsList);
+
+        foreach (json_decode(file_get_contents($file)) as $item) {
+            if (intval($item->price) == 0) continue;
+            if (in_array($item->code, $registeredCodes)) {
+                print "edit product: {$item->code}\n";
+                $product = $productsList[$item->code];
+                $product->price = intval(str_replace(' ', '', $item->price));
+                $product->name = $item->name;
+                $product->description = $item->name;
+                $product->is_by_weight = $item->unit == 'кг';
+                $product->archived = 0;
+                $product->save();
+            } else {
+                print "new product: {$item->code}\n";
+                if (!in_array($item->category, array_keys($categoriesMap))) {
+                    print "new category: {$item->category}\n";
+                    $category = new Categories;
+                    $category->is_imported = 1;
+                    $category->parent_id = 0;
+                    $category->name = $item->category;
+                    $category->save();
+                    $categoriesMap[$item->category] = $category->id;
+                } else {
+                    print "category exist: {$item->category}\n";
+                }
+                $product = new Product;
+                $product->is_imported = 1;
+                $product->category_id = $categoriesMap[$item->category];
+                $product->name = $item->name;
+                $product->description = $item->name;
+                $product->code = $item->code;
+                $product->price = intval(str_replace(' ', '', $item->price));
+                $product->is_by_weight = $item->unit == 'кг';
+                $product->save();
+            }
+            unset($productsList[$item->code]);
+        }
+
+        foreach ($productsList as $item) {
+            if ($item->is_imported) {
+                print "archived: {$item->code}\n";
+                $item->archived = 1;
+                $item->save();
+            }
         }
 
         return 0;

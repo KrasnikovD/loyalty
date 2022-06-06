0f3cf105933862de68c8e09026e5c19001b46027
Eduard <admin@admin.xyz>
2022-06-06 22:15:44 +0300

    ''

--- app/Exports/QuestionExport.php
+++ app/Exports/QuestionExport.php
@@ -2,6 +2,7 @@
 namespace App\Exports;
 
 use App\Models\ClientAnswers;
+use App\Models\Questions;
 use Maatwebsite\Excel\Concerns\FromArray;
 
 class QuestionExport implements FromArray
@@ -15,11 +16,41 @@ class QuestionExport implements FromArray
 
     public function array(): array
     {
-        $q = ClientAnswers::select('cards.user_id', 'questions.news_id', 'cards.number', 'client_answers.value', 'questions.text')
+        $answersData = ClientAnswers::select('questions.text as question_text', 'users.id as user_id', 'users.first_name', 'users.second_name', 'cards.number as card_number', 'client_answers.value as answer', 'client_answers.question_id', 'answer_option_id', 'questions.type')
             ->join('questions', 'questions.id', '=', 'client_answers.question_id')
             ->join('cards', 'cards.user_id', '=', 'client_answers.client_id')
+            ->join('users', 'users.id', '=', 'client_answers.client_id')
+            ->whereNull('questions.deleted_at')
             ->where('questions.news_id', '=', $this->newsId)
-            ->whereIn('questions.type', [1,2]);
-        return $q->get()->toArray();
+            ->get();
+
+        $map1 = [];
+        foreach ($answersData as $datum) {
+            $map1[$datum['user_id'] . '_' . $datum['question_id']] = $datum->toArray();
+        }
+
+        $values = [];
+        foreach ($answersData as $datum) {
+            $userId = $datum['user_id'];
+            $questionId = $datum['question_id'];
+            if (!isset($values[$userId . '_' . $questionId]))
+                $values[$userId . '_' . $questionId] = [];
+            $values[$userId . '_' . $questionId][] = $datum->answer;
+        }
+
+        foreach ($map1 as &$item) {
+            $rawValue = $values[$item['user_id'] . '_'. $item['question_id']];
+            if ($item['type'] == Questions::TYPE_BOOLEAN) {
+                $item['answer'] = $rawValue[0] == 1 ? 'Да' : 'Нет';
+            }
+            if ($item['type'] == Questions::TYPE_OPTIONS) {
+                $item['answer'] = implode(',', $rawValue);
+            }
+        }
+        foreach ($map1 as &$item) {
+            unset($item['type'], $item['answer_option_id'], $item['user_id'], $item['question_id']);
+        }
+        $map1 = array_values($map1);
+        return $map1;
     }
 }
